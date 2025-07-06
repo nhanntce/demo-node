d6889831b415730c9ad43fef84d342c335d3ce63
Luong Van Trung <trunglv@cymosoft.com>
2025-07-06 16:58:45 +0700

    Change port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello from backend NodeJs!' });
 });
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
