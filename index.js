b84134c8aaf75c43c9b133e6db53343c2b281ed5
Luong Van Trung <trunglv@cymosoft.com>
2025-07-06 17:00:54 +0700

    Change port and fix CD

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello from backend NodeJs!' });
 });
 
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log('Server is running on port 3000');
 });
